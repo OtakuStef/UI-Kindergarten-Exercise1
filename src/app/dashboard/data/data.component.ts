2385420b06ba7174d49426d866fbe939266cbec9
websiteriesen <office@websiteriesen.at>
2023-10-09 08:01:05 +0200

    Initial commit

---
+++ src/app/dashboard/data/data.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { BackendService } from 'src/app/shared/backend.service';
+import { SENSORS_PER_PAGE } from 'src/app/shared/constants';
+import { StoreService } from 'src/app/shared/store.service';
+
+@Component({
+  selector: 'app-data',
+  templateUrl: './data.component.html',
+  styleUrls: ['./data.component.scss']
+})
+export class DataComponent implements OnInit {
+
+  constructor(public storeService: StoreService, private backendService: BackendService) {}
+  public currentPage: number = 1;
+  public page: number = 0;
+
+  ngOnInit(): void {
+    this.backendService.getChildren(this.currentPage);
+  }
+
+  getAge(birthDate: string) {
+    var today = new Date();
+    var birthDateTimestamp = new Date(birthDate);
+    var age = today.getFullYear() - birthDateTimestamp.getFullYear();
+    var m = today.getMonth() - birthDateTimestamp.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDateTimestamp.getDate())) {
+        age--;
+    }
+    return age;
+  }
+
+  async selectPage(i: any) {
+    this.currentPage = i + 1;
+    await this.backendService.getChildren(this.currentPage);
+  }
+
+  public returnAllPages() {
+    return Math.ceil(this.storeService.sensorenDatenTotalCount / SENSORS_PER_PAGE)
+  }
+}
+
+
---
+++ src/app/shared/backend.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Kindergarden } from './interfaces/Kindergarden';
+import { StoreService } from './store.service';
+import { Child, ChildResponse } from './interfaces/Child';
+import { SENSORS_PER_PAGE } from './constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BackendService {
+
+  constructor(private http: HttpClient, private storeService: StoreService) { }
+
+  public getKindergardens() {
+    this.http.get<Kindergarden[]>('http://localhost:5000/kindergardens').subscribe(data => {
+      this.storeService.kindergardens = data;
+    });
+  }
+
+  public getChildren(page: number) {
+    this.http.get<ChildResponse[]>(`http://localhost:5000/childs?_expand=kindergarden&_page=${page}&_limit=${SENSORS_PER_PAGE}`, { observe: 'response' }).subscribe(data => {
+      console.log(data);
+      this.storeService.children = data.body!;
+      this.storeService.sensorenDatenTotalCount = Number(data.headers.get('X-Total-Count'));
+
+    });
+    }
+
+    public async addChildData(child: Child, page:  number) {
+      this.http.post('http://localhost:5000/childs', child).subscribe(_ => {
+        this.getChildren(page);
+      })
+    }
+  }
